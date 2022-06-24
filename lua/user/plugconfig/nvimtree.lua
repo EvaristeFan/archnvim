77b0c9f535236e198f5e06188865bd5fc01944a6
EvaristeFan <reluE_L@outlook.com>
2022-06-24 18:04:14 +0800

    Nvimtree config editing

--- lua/user/plugconfig/nvimtree.lua
+++ lua/user/plugconfig/nvimtree.lua
@@ -46,8 +46,8 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
 --    auto_open = true,
 --  },
     update_focused_file = {
-      enable = true,
-      update_cwd = true,
+      enable = false,
+      update_cwd = false,
       ignore_list = {},
     },
 --  ignore_ft_on_setup = {},
