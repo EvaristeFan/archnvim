811c20f45d369706fe2827239f8f0aa8948ff230
EvaristeFan <reluE_L@outlook.com>
2022-08-18 11:40:33 +0800

    Add t map of hop

--- lua/user/plugconfig/hop.lua
+++ lua/user/plugconfig/hop.lua
@@ -4,7 +4,8 @@ require'hop'.setup {}
 
 -- Hop keymaps {{{
 keymap({'o','n','v'}, 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
-keymap({'o','n','v'}, 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
+keymap({'o','n','v'}, 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, hint_offset = 1, current_line_only = true })<cr>", {})
+keymap({'o','n','v'}, 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, hint_offset = -1, current_line_only = true })<cr>", {})
 keymap({'o','n','v'}, 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
 keymap({'o','n','v'}, 'w', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
 keymap({'o','n','v'}, 'e', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_position = require'hop.hint'.HintPosition.END })<cr>", {})
