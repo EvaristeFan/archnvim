ed2c8ee22cb443eff0b33e57a0777d264266b345
EvaristeFan <reluE_L@outlook.com>
2022-08-26 21:41:48 +0800

    Update hop keymap and neovide font

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -14,7 +14,7 @@ local options = {
 	undofile = true,
 	backupdir = homepath.."/Document/backupdir",
 	undodir = homepath.."/Document/undofile",
-	guifont = "FiraCode Nerd font:h12",
+	guifont = "FiraCode Nerd font:h14",
 }
 
 for k, v in pairs(options) do
