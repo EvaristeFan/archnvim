66f39fc139cf3a1e39397c08c89ad0b09902f2d0
EvaristeFan <reluE_L@outlook.com>
2022-10-13 17:22:36 +0800

    Add nvim_surround config file

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -37,21 +37,7 @@ require('packer').startup(function(use)
 	use({
 		"kylechui/nvim-surround",
 		config = function()
-			local surround_config1 = require("nvim-surround.config")
-			require("nvim-surround").setup({
-				surrounds = {
-					["e"] = {
-						add = function()
-							local env_name = surround_config1.get_input("Enter the enviroment name: ")
-							if env_name then
-								return { { "\\begin{" .. env_name .. "}" }, { "\\end{" .. env_name .. "}" } }
-							end
-						end
-					},
-				},
-				move_cursor = false,
-				-- Configuration here, or leave empty to use defaults
-			})
+			require("user.plugconfig.nvim_surround")
 		end
 	}) --}}}
 
