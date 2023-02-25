8f3a9d06da0c0443256898fa1e8b47dac68528f6
EvaristeFan <reluE_L@outlook.com>
2023-02-25 18:09:32 +0800

    Modify firenvim config to build correctly

--- lua/user/lazy/init.lua
+++ lua/user/lazy/init.lua
@@ -211,11 +211,14 @@ return {
 
 	{
 		'glacambre/firenvim',
-		build = function() vim.fn['firenvim#install'](0) end,
 
 		-- Lazy load firenvim
 		-- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-		cond = not not vim.g.started_by_firenvim
+		cond = not not vim.g.started_by_firenvim,
+		build = function()
+			require("lazy").load({ plugins = "firenvim", wait = true })
+			vim.fn["firenvim#install"](0)
+		end
 	},
 }
 
